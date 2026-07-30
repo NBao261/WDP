500e0f21af724d8ad7867175b79a76c6475e26ea
NBao261 <ncb2601@gmail.com>
2026-07-30 15:31:10 +0700

    fix(email): fix type comparison for SMTP_SECURE

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -9,7 +9,7 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
       host: env.SMTP_HOST || 'smtp.gmail.com',
       port: Number(env.SMTP_PORT) || 587,
-      secure: env.SMTP_SECURE === 'true', // true for 465, false for other ports
+      secure: env.SMTP_SECURE,
       auth: {
         user: env.SMTP_USER,
         pass: env.SMTP_PASS,
